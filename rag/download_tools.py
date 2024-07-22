e7aaba8212c5770264f62be0fbae9033f9a66d9f
Sri Vardhamanan <sri00001000@gmail.com>
2024-07-21 18:46:51 -0700

    add download tools

--- rag/download_tools.py
+++ rag/download_tools.py
@@ -2,7 +2,7 @@
 # All rights reserved.
 #
 # This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
+# LICENSE file in the root directory of the ATLAS repo: https://github.com/facebookresearch/atlas.
 
 import os
 
